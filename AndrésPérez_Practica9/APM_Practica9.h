baedd114327b05bc429eed3478aafad71b6e02d9
Adayer <aday3r@gmail.com>
2021-10-29 18:20:01 +0200

    Cosa practica 9

--- AndrésPérez_Practica9/APM_Practica9.h
+++ AndrésPérez_Practica9/APM_Practica9.h
@@ -18,7 +18,9 @@ class CNode
 {
 public:
 	const char* GetChar() { return m_pChar; };
+	void SetChar(const char* _char) { m_pChar = _char; };
+	void SetNext(CNode* _next) { m_Next = _next; };
 private:
 	const char* m_pChar;
-
+	CNode* m_Next;
 };
