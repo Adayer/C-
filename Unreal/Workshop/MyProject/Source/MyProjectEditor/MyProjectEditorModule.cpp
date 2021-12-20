cc802ac480df9944f3a41f529427ade705de8d6e
Adayer <aday3r@gmail.com>
2021-12-20 19:29:13 +0100

    Para porsi

--- Unreal/Workshop/MyProject/Source/MyProjectEditor/MyProjectEditorModule.cpp
+++ Unreal/Workshop/MyProject/Source/MyProjectEditor/MyProjectEditorModule.cpp
@@ -2,12 +2,16 @@
 
 
 #include "MyProjectEditorModule.h"
+#include "ConversationAssetAction.h"
 
 IMPLEMENT_MODULE(FMyProjectEditorModule, MyProjectEditor)
 
 void FMyProjectEditorModule::StartupModule()
 {
-
+	ConversationActions = MakeShareable(new FConversationAssetAction());
+	FAssetToolsModule& AssetToolsMod = FModuleManager::LoadModuleChecked<FAssetToolsModule>("AssetTools");
+	IAssetTools& AssetTools = AssetToolsMod.Get();
+	AssetTools.RegisterAssetTypeActions(ConversationActions.ToSharedRef());
 }
 void FMyProjectEditorModule::ShutdownModule()
 {
--- Unreal/Workshop/MyProject/Source/MyProjectEditor/MyProjectEditorModule.h
+++ Unreal/Workshop/MyProject/Source/MyProjectEditor/MyProjectEditorModule.h
@@ -4,9 +4,13 @@
 #include "Modules/ModuleInterface.h"
 #include "Modules/ModuleManager.h"
 
+class FConversationAssetAction;
+
 class FMyProjectEditorModule : public IModuleInterface
 {
 public:
 	virtual void StartupModule() override;
 	virtual void ShutdownModule() override;
+private:
+	TSharedPtr<FConversationAssetAction> ConversationActions;
 };
