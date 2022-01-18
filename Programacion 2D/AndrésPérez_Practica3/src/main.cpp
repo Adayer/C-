23fba08fb240ec98c000a78a1dffdb7c674cb4da
Adayer <aday3r@gmail.com>
2022-01-18 21:42:44 +0100

    Fuentes no funcionan jeje

--- Programacion 2D/AndrésPérez_Practica3/src/main.cpp
+++ Programacion 2D/AndrésPérez_Practica3/src/main.cpp
@@ -1,7 +1,8 @@
-
+﻿
 #define LITE_GFX_IMPLEMENTATION
 #define STB_IMAGE_IMPLEMENTATION
 #define STB_TRUETYPE_IMPLEMENTATION
+#define _CRT_SECURE_NO_WARNINGS
 
 #include <litegfx.h>
 #include <glfw3.h>
@@ -12,7 +13,7 @@
 #include <string>
 #include "stb_truetype.h"
 
-#pragma region MarcosPractica 2
+#pragma region MacrosPractica 2
 //
 //#define TEXTURE_LIST(_ACTION)\
 //_ACTION(Fire, png)\
@@ -47,13 +48,6 @@ using namespace std;
 
 FILE* font1 = nullptr;
 
-unsigned char bufferFont1[2000];
-unsigned char bufferPixels1[2000];
-
-unsigned char bufferRGB1[10] = {"9"}; //1048576
-
-stbtt_bakedchar bakedCharsData[2000];
-
 int main() {
 
     if (!glfwInit()) {
@@ -71,32 +65,52 @@ int main() {
     CTime time;
 
     //Fonts Init
-    
 
     const char* font1Route = "data/Orange.ttf";
-    fopen_s(&font1, font1Route, "r");
+    fopen_s(&font1, font1Route, "rb");
 
     if (!font1)
     {
         assert(0);
     }
+    fseek(font1, 0, SEEK_END);
+    int tellNum = ftell(font1);
+    fseek(font1, 0, SEEK_SET);
 
-    size_t num = fread(bufferFont1, sizeof(char), 2000, font1);
-    
+    char* bufferFont1 = new char[tellNum];
 
-    int x = 2048;
-    int y = 2048;
+    size_t num = fread(bufferFont1, sizeof(char), tellNum, font1);
+    int cosa2 = fclose(font1);
+
+    unsigned char* bufferPixels1 = new unsigned char[512 * 512];
+    unsigned char* bufferRGB1 = new unsigned char[512 * 512 * 4];
+    stbtt_bakedchar* bakedCharsData = new stbtt_bakedchar[1];
+
+    for (unsigned int i = 0; i < 512 * 512; ++i)
+    {
+        bufferPixels1[i] = 0;
+    }
+    unsigned char* data = reinterpret_cast<unsigned char*>(bufferFont1);
+
+    int cosa = stbtt_BakeFontBitmap(data, 0, 8, bufferPixels1, 512, 512, 0, 255, bakedCharsData);
+
+    for (unsigned int i = 0; i < 512 * 512 * 4; ++i)
+    {
+        bufferRGB1[i] = 255;
+    }
+
+    for (size_t i = 0; i < 512 * 512; ++i)
+    {
+        bufferRGB1[i * 4 + 3] = data[i];
+    }
+
+    delete[] bufferPixels1;
+    delete[] bufferFont1;
+    int x = 512;
+    int y = 512;
     int* pX = &x;
     int* pY = &y;
 
-    stbtt_BakeFontBitmap(bufferFont1, 0, 20, bufferPixels1, 512, 512, 0, 255, bakedCharsData);
-
-    
-    for (size_t i = 0; i < 2000; ++i)
-    {
-        bufferRGB1[i * 4 + 3] = bufferPixels1[i];
-    }
-   
     ltex_t* text_font1 = ltex_alloc(*pX, *pY, 1);
     ltex_setpixels(text_font1, bufferRGB1);
 
@@ -109,23 +123,24 @@ int main() {
 
     while (glfwWindowShouldClose(mainWindow) == 0)
     {
-        time.UpdateDeltaTime();
+        //time.UpdateDeltaTime();
 
-        glfwPollEvents();
-
-        //Escape to close Window
         if (glfwGetKey(mainWindow, GLFW_KEY_ESCAPE))
         {
             glfwSetWindowShouldClose(mainWindow, 1);
         }
 
+        glfwPollEvents();
+
+        //Escape to close Window
         //stbtt_GetBakedQuad(bakedCharsData, 20, 20, 41, pfX, pfY, infoQuad, true);
         //ltex_drawrotsized(text_font1, infoQuad->x0, infoQuad->y0, 0.f, 0.f, 0.f, 60.f, 60.f, infoQuad->s0, infoQuad->t0, infoQuad->s1, infoQuad->t1);
 
-        lgfx_clearcolorbuffer(255, 0, 0);
-        ltex_drawrotsized(text_font1, infoQuad->x0, infoQuad->y0, 0.f, 0.f, 0.f, 512.f, 512.f, 0, 0, 1, 1);
+        //lgfx_clearcolorbuffer(255, 0, 0);
+        //lgfx_setblend(BLEND_ALPHA);
+        //ltex_drawrotsized(text_font1, infoQuad->x0, infoQuad->y0, 0.f, 0.f, 0.f, 512.f, 512.f, 0, 0, 1, 1);
         
-        glfwSwapBuffers(mainWindow);
+        //glfwSwapBuffers(mainWindow);
     }
 
     glfwTerminate();
