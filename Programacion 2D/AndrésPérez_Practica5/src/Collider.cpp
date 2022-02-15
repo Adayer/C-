777c20f56767b56e9ae315a30e2a3d0bfd6e36b0
Adayer <aday3r@gmail.com>
2022-02-15 18:05:27 +0100

    Colision Pixeles Rectangulo terminada

--- Programacion 2D/AndrésPérez_Practica5/src/Collider.cpp
+++ Programacion 2D/AndrésPérez_Practica5/src/Collider.cpp
@@ -1,4 +1,10 @@
 #include "Collider.h"
+#include <algorithm>
+#include <vector>
+
+float Clamp(float n, float lower, float upper) {
+	return std::max(lower, std::min(n, upper));
+}
 
 bool Collider::CheckCircleCircle(const vec2& _pos1, float _radius1,
 	const vec2& _pos2, float _radius2) const
@@ -212,6 +218,55 @@ bool Collider::CheckRectRect(const vec2& _pos1, const vec2& _size1,
 bool Collider::CheckRectPixels(const vec2& _rectPos, const vec2& _rectSize, 
 	const vec2& _pixelPos, const vec2& _pixelsSize, const uint8_t* _pixels) const
 {
+	if (((_rectPos.x < (_pixelPos.x + _pixelsSize.x / 2.f) && _rectPos.x >(_pixelPos.x + _pixelsSize.x / 2.f)) &&
+		(_rectPos.y < (_pixelPos.y + _pixelsSize.y / 2.f) && _rectPos.y >(_pixelPos.y + _pixelsSize.y / 2.f)))
+		||
+		((_pixelPos.x < (_rectPos.x + _rectSize.x / 2.f) && _pixelPos.x >(_rectPos.x + _rectSize.x / 2.f))&&
+			(_pixelPos.y < (_rectPos.y + _rectSize.y / 2.f) && _pixelPos.y >(_rectPos.y + _rectSize.y / 2.f))))
+
+	{
+		//x values
+		std::vector<float> xCrossPossible;
+		//RectPossible Values
+		xCrossPossible.push_back(_rectPos.x - _rectSize.x);
+		xCrossPossible.push_back(_rectPos.x + _rectSize.x);
+		//Pixels possible values
+		xCrossPossible.push_back(_pixelPos.x - _pixelsSize.x);
+		xCrossPossible.push_back(_pixelPos.x + _pixelsSize.x);
+		std::sort(xCrossPossible.begin(), xCrossPossible.end()); //Index 1 & 2 are the good values
+		//Min & Max X del punto de corte
+		float minX = xCrossPossible[1];
+		float maxX = xCrossPossible[2];
+		
+		//y values
+		std::vector<float> yCrossPossible;
+		//RectPossible Values
+		yCrossPossible.push_back(_rectPos.y - _rectSize.y);
+		yCrossPossible.push_back(_rectPos.y + _rectSize.y);
+		//Pixels possible values
+		yCrossPossible.push_back(_pixelPos.y - _pixelsSize.y);
+		yCrossPossible.push_back(_pixelPos.y + _pixelsSize.y);
+		std::sort(yCrossPossible.begin(), yCrossPossible.end()); //Index 1 & 2 are the good values
+		//Min & Max Y del punto de corte en valor absoluto de pantalla
+		float minY = yCrossPossible[1];
+		float maxY = yCrossPossible[2];
+
+		//Puntos de corte finales en valor relativo al centro de los pixeles
+		vec2 minCrossing(minX - _pixelsSize.x, minY - _pixelsSize.y);
+		vec2 maxCrossing(maxX - _pixelsSize.x, maxY - _pixelsSize.y);
+
+		for (unsigned int y = minCrossing.y; y <= maxCrossing.y; ++y)
+		{
+			for (unsigned int x = minCrossing.x; x <= maxCrossing.x; ++x)
+			{
+				int pixelAlpha = ((y * _pixelsSize.x + x) * 4) - 1;
+				if (_pixels[pixelAlpha] != 0)
+				{
+					return true;
+				}
+			}
+		}
+	}
 	return false;
 }
 bool Collider::CheckPixelsPixels(const vec2& _pos1, const vec2& _size1, const uint8_t* _pixels1,
